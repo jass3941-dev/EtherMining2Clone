757a3d8edb15d69734f43f14b96fc7711a45c88a
jasss3941 <51388846+jasss3941@users.noreply.github.com>
2019-07-30 14:21:21 +0900

    Update startup.sh

--- 20190730_jwlee/Linux/startup.sh
+++ 20190730_jwlee/Linux/startup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./ethminer -v 3 -G -P stratum2+tcp://jasss3941%2eminertest:x@asia1.ethereum.miningpoolhub.com:20535 --cl-local-work 256 >>  miner.log
+./ethminer -v 3 -G -P stratum2+tcp://jasss3941%2eminertest:x@asia1.ethereum.miningpoolhub.com:20535 --cl-local-work 256
