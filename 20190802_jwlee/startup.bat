527473d87317eadaa134639490f658e227f262b7
jasss3941 <51388846+jasss3941@users.noreply.github.com>
2019-08-04 10:18:53 +0900

    Update startup.bat
    Global Work Memory 설정을 Adjust한다.

--- 20190802_jwlee/startup.bat
+++ 20190802_jwlee/startup.bat
@@ -1,1 +1,1 @@
-ethminer.exe -G -P stratum2+tcp://jasss3941%%2eminertest:x@asia1.ethereum.miningpoolhub.com:20535 --opencl-platform 1 --cl-local-work 256
+ethminer.exe -G -P stratum2+tcp://jasss3941%%2eminertest:x@asia1.ethereum.miningpoolhub.com:20535 --opencl-platform 1 --cl-local-work 256 
